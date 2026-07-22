da2ba704ec0cff0788eb90a7df075938fa504d84
Adams Conan Doyle <3662308525@qq.com>
2026-07-22 16:19:22 +0800

    Update log message to remove sensitive URL

--- StoleWebSite4/src/main/java/org/designer/StoleWebSite4Application.java
+++ StoleWebSite4/src/main/java/org/designer/StoleWebSite4Application.java
@@ -12,7 +12,7 @@ public class StoleWebSite4Application {
 
     public static void main(String[] args) {
         SpringApplication.run(StoleWebSite4Application.class, args);
-        log.info("攻击https://593937.kkxk52.shop/play/450936-1-1/");
+        log.info("攻击https:/xxx/");
         log.info("http://localhost:8081。");
     }
 
